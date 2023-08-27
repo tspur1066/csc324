c30ac04ff18c5f9fe4fe4f097699408b12b2f095
Aristotleus <tspur1@protonmail.com>
2023-08-27 14:46:39 -0400

    Added Exercise 1

---
+++ ch02_thomas_spurlock.js
@@ -0,0 +1,24 @@
+// Chapter 2 Homework
+// Thomas Spurlock
+// Due: 8/31/2023
+
+// Problem #1: Looping a triangle
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+//It may be useful to know that you can find the length of a string by writing .length after it.
+
+// let abc = "abc";
+// console.log(abc.length);
+//  → 3
+
+// Solution #1: 
+
+
