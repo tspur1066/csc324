e27c8628458761a69f27527d64fd33c2f32db6cd
Aristotleus <tspur1@protonmail.com>
2023-08-29 16:28:02 -0400

    Updating Chapter 2 HW

--- ch02_thomas_spurlock.js
+++ ch02_thomas_spurlock.js
@@ -13,11 +13,13 @@
 // ######
 // #######
 
-//It may be useful to know that you can find the length of a string by writing .length after it.
+// It may be useful to know that you can find the length of a string by writing .length after it.
 
 // let abc = "abc";
 // console.log(abc.length);
 //  → 3
+
+
 
 // Solution #1: 
 
@@ -33,10 +35,44 @@ for (let length = symbol.length; symbol.length <= 7; symbol += "#") {
 
 // Write a program that uses console.log to print all the numbers from 1 to 100,
 // with two exceptions. For numbers divisible by 3, print "Fizz" 
-//instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+// instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 
-// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+// When you have that working, modify your program to print "FizzBuzz"
+// for numbers that are divisible by both 3 and 5 
+//(and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
-// (This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
+// (This is actually an interview question that has been claimed to weed out a significant
+// percentage of programmer candidates. So if you solved it, your labor market value just went up.)
 
+
+
+// Solution #2
+
+let counter = 0;
+while (counter <= 100) {
+    if (counter % )
+}
+
+
+
+
+
+// Problem #3
+
+// Chessboard
+
+// Write a program that creates a string that represents an 8×8 grid,
+// using newline characters to separate lines. At each position of the grid there
+// is either a space or a "#" character. The characters should form a chessboard.
+
+// Passing this string to console.log should show something like this:
+
+// # # # #
+// # # # # 
+// # # # #
+// # # # # 
+// # # # #
+// # # # # 
+// # # # #
+// # # # #
 
