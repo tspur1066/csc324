85e3d3a4ebe4260edbf7b4acb3ecbdc9fbf69dd4
Aristotleus <tspur1@protonmail.com>
2023-08-29 16:16:15 -0400

    Updating Chapter 2 HW

--- ch02_thomas_spurlock.js
+++ ch02_thomas_spurlock.js
@@ -21,6 +21,22 @@
 
 // Solution #1: 
 
-console.log()
+let symbol = "#";
+for (let length = symbol.length; symbol.length <= 7; symbol += "#") {
+    console.log(symbol)
+}
+
+
+
+
+// Problem # 2: FizzBuzz
+
+// Write a program that uses console.log to print all the numbers from 1 to 100,
+// with two exceptions. For numbers divisible by 3, print "Fizz" 
+//instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+// (This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
 
 
--- scratch.js
+++ scratch.js
@@ -16,3 +16,25 @@ console.log("#");
 
 let number = symb.length
 
+let counter = 0
+let length = 
+while (counter <= 7) {
+console.log("#")
+}
+
+for (let counter = 0; counter <= 7; counter += 1 ) {
+    console.log(counter);
+}
+
+let symbol = "#";
+for (let length = symbol.length; symbol.length <= 7; symbol += "#") {
+    console.log(symbol)
+}
+
+console.log("#"+2("#"))
+
+
+
+
+
+
