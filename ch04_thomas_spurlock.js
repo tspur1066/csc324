93a5fb5cd430376cac44abc7de6677097f46c677
Aristotleus <tspur1@protonmail.com>
2023-09-07 15:05:28 -0400

    Updating Chapter 3 HW

--- ch04_thomas_spurlock.js
+++ ch04_thomas_spurlock.js
@@ -11,6 +11,7 @@
 
 console.log(sum(range(1, 10)));
 
+
 // Write a range function that takes two arguments, start and end,
 // and returns an array containing all the numbers from start up to (and including) end.
 
@@ -22,6 +23,28 @@ console.log(sum(range(1, 10)));
 // the elements go up by increments of one, corresponding to the old behavior. 
 // The function call range(1, 10, 2) should return [1, 3, 5, 7, 9].
 // Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+
+
+// Solution #1
+
+
+
+
+
+// Problem #2 - Reversing an Array
+
+// Arrays have a reverse method that changes the array by inverting the order in which its elements appear. 
+// For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+// The first, reverseArray, takes an array as argument and produces a new array
+// that has the same elements in the inverse order. The second, reverseArrayInPlace,
+// does what the reverse method does: it modifies the array given as argument by reversing its elements. 
+// Neither may use the standard reverse method.
+
+// Thinking back to the notes about side effects and pure functions in the previous chapter, 
+// which variant do you expect to be useful in more situations? Which one runs faster?
+
+
+// Solution #2
 
 
 
