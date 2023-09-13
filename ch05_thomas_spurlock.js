52cabd8971f037077f370793d397cc1a2c0de27e
Aristotleus <tspur1@protonmail.com>
2023-09-13 15:47:13 -0400

    Updating Chapter 4 HW, and also Creating Chapter 5 HW file

---
+++ ch05_thomas_spurlock.js
@@ -0,0 +1,46 @@
+// Thomas Spurlock
+// Chapter 5 HW
+// Due: September 21st, 2023
+
+
+
+// Problem #1 - Flattening
+
+// Use the reduce method in combination with the concat method to “flatten” an array of arrays
+// into a single array that has all the elements of the original arrays.
+
+
+// Solution #1
+
+
+
+
+
+// Problem #2 - Your own for loop
+
+// Write a higher-order function loop that provides something like a for loop statement.
+// It takes a value, a test function, an update function, and a body function. 
+// Each iteration, it first runs the test function on the current loop value and stops if that returns false.
+// Then it calls the body function, giving it the current value. 
+// Finally, it calls the update function to create a new value and starts from the beginning.
+
+// When defining the function, you can use a regular loop to do the actual looping.
+
+
+// Solution #2
+
+
+
+
+
+// Problem #3 - Everything
+
+// Analogous to the some method, arrays also have an every method. 
+// This one returns true when the given function returns true for every element in the array.
+// In a way, some is a version of the || operator that acts on arrays, and every is like the && operator.
+
+// Implement every as a function that takes an array and a predicate function as parameters.
+// Write two versions, one using a loop and one using the some method.
+
+
+// Solution #3
