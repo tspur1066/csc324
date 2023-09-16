bcbfa285f8bf9aff4b47ded0463fab2bd3df1498
Aristotleus <tspur1@protonmail.com>
2023-09-16 16:42:20 -0400

    Updating Chapter 5 HW

--- CSC324 HW/ch05_thomas_spurlock.js
+++ CSC324 HW/ch05_thomas_spurlock.js
@@ -12,7 +12,17 @@
 
 // Solution #1
 
-
+let flattener = function(array) {
+    let place = [];
+    for (let i = 0; i < array.length; i += 1) {
+        new_array = array.reduce((place, function (a, b) {
+            return place.concat(a, b);
+        }));
+    }
+    return new_array;
+}
+console.log(flattener([[1, 2, 3], [1, 2, 3]]));
+console.log(flattener([[1, 3, 5], [7, 9, 11]]));
 
 
 
